2f2204c86ebd5a1037ee08ac6ec57980573eaee2
Pubudu Sanka <pubudusanka79@gmail.com>
2025-10-02 12:20:49 +0530

    update field setting

--- backend/hotel-management-service-api/src/main/java/com/hotel_management/hotel_management_service_api/entity/Branch.java
+++ backend/hotel-management-service-api/src/main/java/com/hotel_management/hotel_management_service_api/entity/Branch.java
@@ -1,15 +1,30 @@
 package com.hotel_management.hotel_management_service_api.entity;
 
+import com.hotel_management.hotel_management_service_api.enums.BranchType;
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "branch")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Branch {
+
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String branchId;
 
+    @Column(name = "room_count", nullable = false)
     private int roomCount;
-    
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "branch_type", nullable = false)
+    private BranchType branchType;
+
+    @Column(name = "branch_name", nullable = false, length = 150)
+    private String branchName;
 
 }
