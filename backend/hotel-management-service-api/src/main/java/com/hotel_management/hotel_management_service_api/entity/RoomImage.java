bf46645aa0b00df3550bcd3589b505200cc07124
Pubudu Sanka <pubudusanka79@gmail.com>
2025-10-02 12:33:44 +0530

    room image create with file formatter

---
+++ backend/hotel-management-service-api/src/main/java/com/hotel_management/hotel_management_service_api/entity/RoomImage.java
@@ -0,0 +1,23 @@
+package com.hotel_management.hotel_management_service_api.entity;
+
+import com.hotel_management.hotel_management_service_api.util.FileFormatter;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "room_image")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RoomImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String roomImageId;
+
+    @Embedded
+    private FileFormatter fileFormatter;
+    
+}
---
+++ backend/hotel-management-service-api/src/main/java/com/hotel_management/hotel_management_service_api/util/FileFormatter.java
@@ -0,0 +1,22 @@
+package com.hotel_management.hotel_management_service_api.util;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Lob;
+
+@Embeddable
+public class FileFormatter {
+
+    @Lob
+    @Column(name = "file_name")
+    private byte[] fileName;
+
+    @Column(name = "resource_url")
+    private byte[] resourceUrl;
+
+    @Column(name = "directory")
+    private byte[] directory;
+
+    @Column(name = "hash")
+    private byte[] hash;
+}
