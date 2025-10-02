4311414e20b7dd5b2377386a6d5982ec0c780732
Pubudu Sanka <pubudusanka79@gmail.com>
2025-10-02 13:27:14 +0530

    Update ResponseHotelPaginationDto.java

--- backend/hotel-management-service-api/src/main/java/com/hotel_management/hotel_management_service_api/dto/response/pagination/ResponseHotelPaginationDto.java
+++ backend/hotel-management-service-api/src/main/java/com/hotel_management/hotel_management_service_api/dto/response/pagination/ResponseHotelPaginationDto.java
@@ -1,4 +1,15 @@
 package com.hotel_management.hotel_management_service_api.dto.response.pagination;
 
+import com.hotel_management.hotel_management_service_api.dto.response.ResponseHotelDto;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
 public class ResponseHotelPaginationDto {
+
+    private List<ResponseHotelDto> dataList;
+    private long dataCount;
 }
