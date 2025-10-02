8fb32aa863aa02c47f30c596163625e03db7c0ab
Pubudu Sanka <pubudusanka79@gmail.com>
2025-10-02 13:07:56 +0530

    modify added with builder annotation

--- backend/hotel-management-service-api/src/main/java/com/hotel_management/hotel_management_service_api/dto/request/RequestBranchDto.java
+++ backend/hotel-management-service-api/src/main/java/com/hotel_management/hotel_management_service_api/dto/request/RequestBranchDto.java
@@ -1,9 +1,11 @@
 package com.hotel_management.hotel_management_service_api.dto.request;
 
 import com.hotel_management.hotel_management_service_api.enums.BranchType;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class RequestBranchDto {
 
     private String branchName;
--- backend/hotel-management-service-api/src/main/java/com/hotel_management/hotel_management_service_api/dto/request/RequestFacilityDto.java
+++ backend/hotel-management-service-api/src/main/java/com/hotel_management/hotel_management_service_api/dto/request/RequestFacilityDto.java
@@ -1,8 +1,10 @@
 package com.hotel_management.hotel_management_service_api.dto.request;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class RequestFacilityDto {
 
     private String facilityName;
