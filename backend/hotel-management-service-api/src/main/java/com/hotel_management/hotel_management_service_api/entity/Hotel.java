180eaf00a4eb24ea49bc9122f5f25e009c34cede
Pubudu Sanka <pubudusanka79@gmail.com>
2025-10-02 12:11:10 +0530

    Create Hotel.java

---
+++ backend/hotel-management-service-api/src/main/java/com/hotel_management/hotel_management_service_api/entity/Hotel.java
@@ -0,0 +1,44 @@
+package com.hotel_management.hotel_management_service_api.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "hotel")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Hotel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String hotelId;
+
+    @Column(name = "hotel_name")
+    private String hotelName;
+
+    @Column(name = "star_rating")
+    private int starRating;
+
+    @Column(name = "description")
+    @Lob
+    private Blob description;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "active_status")
+    private boolean activeStatus;
+
+    @Column(name = "starting_from")
+    private BigDecimal startingFrom;
+}
