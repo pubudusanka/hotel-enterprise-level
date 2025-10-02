1225dd564fccb14c5cecb85c98b5a99c1bb81b8a
Pubudu Sanka <pubudusanka79@gmail.com>
2025-10-03 01:16:57 +0530

    branch endpoints created

--- backend/hotel-management-service-api/src/main/java/com/hotel_management/hotel_management_service_api/api/BranchController.java
+++ backend/hotel-management-service-api/src/main/java/com/hotel_management/hotel_management_service_api/api/BranchController.java
@@ -1,7 +1,6 @@
 package com.hotel_management.hotel_management_service_api.api;
 
 import com.hotel_management.hotel_management_service_api.dto.request.RequestBranchDto;
-import com.hotel_management.hotel_management_service_api.dto.request.RequestHotelDto;
 import com.hotel_management.hotel_management_service_api.service.BranchService;
 import com.hotel_management.hotel_management_service_api.util.StandardResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -50,6 +49,42 @@ public class BranchController {
                         204, "Branch Deleted Successfully!", null
                 ),
                 HttpStatus.NO_CONTENT
+        );
+    }
+
+    @GetMapping("/visitor/find-by-id/{id}")
+    public ResponseEntity<StandardResponseDto> findBranchById(@PathVariable("id") String branchId) throws SQLException {
+        return new ResponseEntity<>(
+                new StandardResponseDto(
+                        200, "Branch Found!", branchService.findByBranchId(branchId)
+                ),
+                HttpStatus.OK
+        );
+    }
+
+    @GetMapping("/visitor/find-all")
+    public ResponseEntity<StandardResponseDto> findAllBranches(
+            @RequestParam String searchText,
+            @RequestParam int page,
+            @RequestParam int size){
+        return new ResponseEntity<>(
+                new StandardResponseDto(
+                        200, "List of the Branches!", branchService.findBranches(page,size,searchText)
+                ),
+                HttpStatus.OK
+        );
+    }
+
+    @GetMapping("/visitor/find-by-hotel-id")
+    public ResponseEntity<StandardResponseDto> findAllBranchesByHotelId(
+            @RequestParam String hotelId,
+            @RequestParam int page,
+            @RequestParam int size){
+        return new ResponseEntity<>(
+                new StandardResponseDto(
+                        200, "List of the Branches!", branchService.findBranchesByHotelId(page,size,hotelId)
+                ),
+                HttpStatus.OK
         );
     }
 }
