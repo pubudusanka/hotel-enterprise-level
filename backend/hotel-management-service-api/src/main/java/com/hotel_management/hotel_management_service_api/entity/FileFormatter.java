95b3af11d507718d818a871ceef1e1c55240379b
Pubudu Sanka <pubudusanka79@gmail.com>
2025-10-03 00:36:12 +0530

    class location changed

--- backend/hotel-management-service-api/src/main/java/com/hotel_management/hotel_management_service_api/entity/FileFormatter.java
+++ backend/hotel-management-service-api/src/main/java/com/hotel_management/hotel_management_service_api/entity/FileFormatter.java
@@ -1,10 +1,18 @@
-package com.hotel_management.hotel_management_service_api.util;
+package com.hotel_management.hotel_management_service_api.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.Lob;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class FileFormatter {
 
     @Lob
