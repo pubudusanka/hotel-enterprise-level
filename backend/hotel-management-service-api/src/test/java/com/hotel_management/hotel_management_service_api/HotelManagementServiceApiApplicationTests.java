b50ec22e319c30efdfeb8c6b59fc2f342f2c92aa
Pubudu Sanka <pubudusanka79@gmail.com>
2025-10-02 11:11:54 +0530

    hotel management service module added

---
+++ backend/hotel-management-service-api/src/test/java/com/hotel_management/hotel_management_service_api/HotelManagementServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel_management.hotel_management_service_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
