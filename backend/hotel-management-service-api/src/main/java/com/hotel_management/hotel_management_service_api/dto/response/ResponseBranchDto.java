df512a67d17e72a50c7c6de18d9588984ca728e6
Pubudu Sanka <pubudusanka79@gmail.com>
2025-10-03 00:37:57 +0530

    response branch modify

--- backend/hotel-management-service-api/src/main/java/com/hotel_management/hotel_management_service_api/dto/response/ResponseBranchDto.java
+++ backend/hotel-management-service-api/src/main/java/com/hotel_management/hotel_management_service_api/dto/response/ResponseBranchDto.java
@@ -1,10 +1,13 @@
 package com.hotel_management.hotel_management_service_api.dto.response;
 
+import com.hotel_management.hotel_management_service_api.entity.Room;
 import com.hotel_management.hotel_management_service_api.enums.BranchType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -17,4 +20,7 @@ public class ResponseBranchDto {
     private BranchType branchType;
     private int roomCount;
     private String hotelId;
+    private String addressId;
+
+    private List<ResponseRoomDto> rooms;
 }
