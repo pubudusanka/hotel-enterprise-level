71f1236a509ae5e9a14a6fd199cf924c4fd1e23d
Pubudu Sanka <pubudusanka79@gmail.com>
2025-10-02 12:28:00 +0530

    Create Facility.java

---
+++ backend/hotel-management-service-api/src/main/java/com/hotel_management/hotel_management_service_api/entity/Facility.java
@@ -0,0 +1,21 @@
+package com.hotel_management.hotel_management_service_api.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "facility")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Facility {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String facilityId;
+
+    @Column(name = "facility_name")
+    private String facilityName;
+}
