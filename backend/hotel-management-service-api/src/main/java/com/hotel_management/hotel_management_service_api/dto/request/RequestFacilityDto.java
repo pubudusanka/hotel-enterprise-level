6d2b6c1f3aa0ce3efe3ec21cf12359c3f8ce18e6
Pubudu Sanka <pubudusanka79@gmail.com>
2025-10-02 12:48:25 +0530

    request facility dto

---
+++ backend/hotel-management-service-api/src/main/java/com/hotel_management/hotel_management_service_api/dto/request/RequestFacilityDto.java
@@ -0,0 +1,10 @@
+package com.hotel_management.hotel_management_service_api.dto.request;
+
+import lombok.Data;
+
+@Data
+public class RequestFacilityDto {
+
+    private String facilityName;
+    private String roomId;
+}
