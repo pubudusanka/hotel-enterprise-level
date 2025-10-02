0d18ef5021e0d0c0ba934b62680223fabfc05937
Pubudu Sanka <pubudusanka79@gmail.com>
2025-10-03 00:36:55 +0530

    exception handle

--- backend/hotel-management-service-api/src/main/java/com/hotel_management/hotel_management_service_api/service/BranchService.java
+++ backend/hotel-management-service-api/src/main/java/com/hotel_management/hotel_management_service_api/service/BranchService.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 
 public interface BranchService {
     public void createBranch(RequestBranchDto data) throws SQLException;
-    public void updateBranch(RequestBranchDto data, String branchId);
+    public void updateBranch(RequestBranchDto data, String branchId) throws SQLException;
     public void deleteBranch(String branchId);
     public ResponseBranchDto findByBranchId(String branchId) throws SQLException;
     public ResponseBranchPaginationDto findBranches(int page, int size, String searchText);
