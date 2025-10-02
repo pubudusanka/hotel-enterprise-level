5f5c81961f8c900a512a7e952ed7dbe3c2c58aec
Pubudu Sanka <pubudusanka79@gmail.com>
2025-10-02 18:16:22 +0530

    blob (byte) handler generated

--- backend/hotel-management-service-api/src/main/java/com/hotel_management/hotel_management_service_api/service/Impl/HotelServiceImpl.java
+++ backend/hotel-management-service-api/src/main/java/com/hotel_management/hotel_management_service_api/service/Impl/HotelServiceImpl.java
@@ -3,7 +3,10 @@ package com.hotel_management.hotel_management_service_api.service.Impl;
 import com.hotel_management.hotel_management_service_api.dto.request.RequestHotelDto;
 import com.hotel_management.hotel_management_service_api.dto.response.ResponseHotelDto;
 import com.hotel_management.hotel_management_service_api.dto.response.pagination.ResponseHotelPaginationDto;
+import com.hotel_management.hotel_management_service_api.entity.Hotel;
 import com.hotel_management.hotel_management_service_api.service.HotelService;
+
+import java.util.UUID;
 
 public class HotelServiceImpl implements HotelService {
     @Override
@@ -29,5 +32,15 @@ public class HotelServiceImpl implements HotelService {
     @Override
     public ResponseHotelPaginationDto findHotels(int page, int size, String searchText) {
         return null;
+    }
+
+    //dto mapped to the entity
+    private Hotel toHotel(RequestHotelDto dto){
+        return dto == null ? null:
+                Hotel.builder()
+                        .hotelName(dto.getHotelName())
+                        .starRating(dto.getStarRating())
+                        .description()
+                        .build();
     }
 }
