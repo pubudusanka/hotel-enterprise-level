f8d32d84947f1e36be06e41074a88a2f4750a63f
Pubudu Sanka <pubudusanka79@gmail.com>
2025-10-03 22:52:54 +0530

    dependency changed because system receiver says updated it

--- backend/hotel-management-service-gateway/src/main/java/com/hotel_management/hotel_management_service_gateway/HotelManagementServiceGatewayApplication.java
+++ backend/hotel-management-service-gateway/src/main/java/com/hotel_management/hotel_management_service_gateway/HotelManagementServiceGatewayApplication.java
@@ -2,8 +2,10 @@ package com.hotel_management.hotel_management_service_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class HotelManagementServiceGatewayApplication {
 
 	public static void main(String[] args) {
