ce8fb6bb3c7012212599ee166f82adba259e4356
Pubudu Sanka <pubudusanka79@gmail.com>
2025-10-03 22:41:48 +0530

    gateway service module created and add gateway dependency

---
+++ backend/hotel-management-service-gateway/src/main/java/com/hotel_management/hotel_management_service_gateway/HotelManagementServiceGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.hotel_management.hotel_management_service_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementServiceGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagementServiceGatewayApplication.class, args);
+	}
+
+}
---
+++ backend/hotel-management-service-gateway/src/test/java/com/hotel_management/hotel_management_service_gateway/HotelManagementServiceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel_management.hotel_management_service_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementServiceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
