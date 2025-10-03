0c20d85e0366873002f2b5c180c68d90fed0eb15
Pubudu Sanka <pubudusanka79@gmail.com>
2025-10-03 23:36:58 +0530

    enable discovery client

--- backend/hotel-management-service-auth-service/src/main/java/com/hotel_management/hotel_management_service_auth_service/HotelManagementServiceAuthServiceApplication.java
+++ backend/hotel-management-service-auth-service/src/main/java/com/hotel_management/hotel_management_service_auth_service/HotelManagementServiceAuthServiceApplication.java
@@ -2,8 +2,10 @@ package com.hotel_management.hotel_management_service_auth_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class HotelManagementServiceAuthServiceApplication {
 
 	public static void main(String[] args) {
