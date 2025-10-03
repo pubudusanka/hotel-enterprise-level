23109c88dcc045c6648574eba6ae688278cc690d
Pubudu Sanka <pubudusanka79@gmail.com>
2025-10-03 23:34:56 +0530

    auth service module

---
+++ backend/hotel-management-service-auth-service/src/main/java/com/hotel_management/hotel_management_service_auth_service/HotelManagementServiceAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hotel_management.hotel_management_service_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementServiceAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagementServiceAuthServiceApplication.class, args);
+	}
+
+}
---
+++ backend/hotel-management-service-auth-service/src/test/java/com/hotel_management/hotel_management_service_auth_service/HotelManagementServiceAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel_management.hotel_management_service_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementServiceAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
