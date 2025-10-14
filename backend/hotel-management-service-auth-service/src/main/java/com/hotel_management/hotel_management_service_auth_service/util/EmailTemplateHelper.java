a58d8362756a2eb0db419e666080ba38c596ea32
Pubudu Sanka <pubudusanka79@gmail.com>
2025-10-14 22:53:51 +0530

    email otp verify
    create template and template helper for otp verification via email

---
+++ backend/hotel-management-service-auth-service/src/main/java/com/hotel_management/hotel_management_service_auth_service/util/EmailTemplateHelper.java
@@ -0,0 +1,21 @@
+package com.hotel_management.hotel_management_service_auth_service.util;
+
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class EmailTemplateHelper {
+
+    public String loadHtmlTemplate(String templateName) {
+        byte[] fileData;
+        try {
+            ClassPathResource resource = new ClassPathResource(templateName);
+            fileData = resource.getInputStream().readAllBytes();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new String(fileData, StandardCharsets.UTF_8);
+    }
+}
