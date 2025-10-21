dc7cb30c0cd1fb2cbd731cb8e52dc819ea433de3
Pubudu Sanka <pubudusanka79@gmail.com>
2025-10-22 01:43:23 +0530

    button bar impl

--- frontend/hotel-client/src/app/pages/home-page/home-page.component.ts
+++ frontend/hotel-client/src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {MainHeaderComponent} from './inner-items/main-header/main-header.component';
+import {OptionsBarComponent} from './inner-items/options-bar/options-bar.component';
 
 @Component({
   selector: 'app-home-page',
   imports: [
-    MainHeaderComponent
+    MainHeaderComponent,
+    OptionsBarComponent
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
---
+++ frontend/hotel-client/src/app/pages/home-page/inner-items/options-bar/options-bar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {MatButton} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-options-bar',
+  imports: [
+    MatButton,
+    MatIcon
+  ],
+  templateUrl: './options-bar.component.html',
+  styleUrl: './options-bar.component.scss',
+  standalone: true
+})
+export class OptionsBarComponent {
+
+}
