3f92efac1a09cb04c04380d32e3097179c870456
Pubudu Sanka <pubudusanka79@gmail.com>
2025-10-28 14:28:56 +0530

    stays form created

--- frontend/hotel-client/src/app/pages/home-page/inner-items/stays-form/stays-form.component.ts
+++ frontend/hotel-client/src/app/pages/home-page/inner-items/stays-form/stays-form.component.ts
@@ -1,11 +1,15 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {MatIcon} from '@angular/material/icon';
 import {provideNativeDateAdapter} from '@angular/material/core';
+import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
 
 @Component({
   selector: 'app-stays-form',
   imports: [
     MatIcon,
+    MatMenu,
+    MatMenuItem,
+    MatMenuTrigger,
   ],
   providers: [provideNativeDateAdapter()],
   changeDetection: ChangeDetectionStrategy.OnPush,
