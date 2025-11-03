a43d7fb1bed7f01cbeedcf5966e39baa4ab675ba
Pubudu Sanka <pubudusanka79@gmail.com>
2025-11-03 21:52:46 +0530

    create dashboard page

---
+++ frontend/admin/app/dashboard/page.jsx
@@ -0,0 +1,21 @@
+'use client';
+import { useRouter } from "next/navigation";
+
+export default function DashboardPage() {
+
+    const router = useRouter();
+
+        function handleLogout(){
+            router.push('/');
+        }
+
+    return(
+        <div className="w-full max-w-sm bg-white rounded-xl shadow p-6 space-y-4">
+            <div className="flex justify-between items-center">
+                <h1 className="text-2xl font-bold mx-1">Dashboard</h1>
+                <button onClick={handleLogout} className="px-4 py-2 bg-gray-200 rounded-lg hover:bg-gray-300">Logout</button>
+                <p className="mx-1">Welcome to Dashboard!</p>
+            </div>
+        </div>
+    );
+}
