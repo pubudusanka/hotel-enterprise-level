923f2f318cb4edb837c5def595caff7d5fcfe45f
Pubudu Sanka <pubudusanka79@gmail.com>
2025-11-03 22:35:44 +0530

    dashboard main content

--- frontend/admin/app/dashboard/page.tsx
+++ frontend/admin/app/dashboard/page.tsx
@@ -10,10 +10,10 @@ export default function DashboardPage() {
         }
 
     return(
-        <div className="w-full max-w-sm bg-white rounded-xl shadow p-6 space-y-4">
+        <div className="w-full h-screen max-w-sm bg-white rounded-xl shadow p-6 space-y-4">
             <div className="flex justify-between items-center">
                 <h1 className="text-2xl font-bold mx-1">Dashboard</h1>
-                <button onClick={handleLogout} className="px-4 py-2 bg-gray-200 rounded-lg hover:bg-gray-300">Logout</button>
+                <button onClick={handleLogout} className="px-4 py-2 bg-gray-200 rounded-lg hover:bg-gray-300 cursor-pointer">Logout</button>
                 <p className="mx-1">Welcome to Dashboard!</p>
             </div>
         </div>
